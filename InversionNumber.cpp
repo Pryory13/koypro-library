2ca9f0876342c4afcdb38a5847505038d8158b47
Pryory13 <Pryory13>
2022-04-19 08:43:18 +0900

    ntt

---
+++ InversionNumber.cpp
@@ -0,0 +1,18 @@
+#include <atcoder/all>
+#include <atcoder/fenwicktree.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+ll InversionNumber(vector<ll> a) {
+    ll n = a.size();
+    fenwick_tree<ll> fw(100005);
+    ll ans = 0;
+    rep(j, n) {
+        ans += j - fw.sum(0, a[j]);
+        fw.add(a[j], 1);
+    }
+    return ans;
+}
